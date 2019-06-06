850d778e49dd876fae1bdce2e279ad8fba4ef74d
Christopher <christophermms.ctt@gmail.com>
2019-06-06 15:01:48 -0300

    Comit_Inicial
    Todos os arquivos .py e jungle instalados

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # apiJungle
+Projeto de API Jungle Rest.
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/christopher/Documentos/criandoapi/apiJungle/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
